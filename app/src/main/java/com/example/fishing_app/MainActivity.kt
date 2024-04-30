371fd91016eda68e1e18228b728646480b55eac5
dyksiu <maciekdyks@wp.pl>
2024-04-30 22:26:21 +0200

    add 3 buttons

--- app/src/main/java/com/example/fishing_app/MainActivity.kt
+++ app/src/main/java/com/example/fishing_app/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +16,20 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val button1 = findViewById<Button>(R.id.button1)
+        val button2 = findViewById<Button>(R.id.button2)
+        val button3 = findViewById<Button>(R.id.button3)
+
+        button1.setOnClickListener {
+            // Kod do wykonania po kliknięciu przycisku 1
+        }
+        button2.setOnClickListener {
+            // Kod do wykonania po kliknięciu przycisku 2
+        }
+        button3.setOnClickListener {
+            // Kod do wykonania po kliknięciu przycisku 3
         }
     }
 }
